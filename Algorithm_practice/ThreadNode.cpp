db5b19099301a2de47e8b40d8ecb738408060170
Fly <13384911233@163.com>
2025-11-10 21:39:37 +0800

    added ThreadTree's functions

--- Algorithm_practice/ThreadNode.cpp
+++ Algorithm_practice/ThreadNode.cpp
@@ -2,6 +2,8 @@
 using namespace std;
 
 typedef char ElemType;
+char str[] = "ABDH##I##EJ###CF##G##";
+int inx = 0;
 
 typedef struct ThreadNode
 {
@@ -10,36 +12,111 @@ typedef struct ThreadNode
     ThreadNode *rchild;
     int ltag;
     int rtag;
-    ThreadNode()
-    {
-        lchild = nullptr;
-        rchild = nullptr;
-        ltag = 0;
-        rtag = 0;
-        data = 0;
-    }
-    ThreadNode(ElemType data)
-    {
-        this->data = data;
-        lchild = nullptr;
-        rchild = nullptr;
-        ltag = 0;
-        rtag = 0;
-    }
 }ThreadNode;
 
 typedef ThreadNode *ThreadTree;
+ThreadTree pre = nullptr;
 
-char str[] = "ABDH##I##EJ###CF##G##";
-int inx = 0;
+//创建树
+void createTree(ThreadTree *T);
 
-ThreadTree prev;
+//中序线索化
+void inOrderThreading(ThreadTree *head,ThreadTree T);
+
+//具体线索化
+void threading(ThreadTree T);
+
+// 使用线索进行中序遍历
+void inOrder(ThreadTree T);
 
 int main()
 {
-    ThreadTree head;
-    ThreadTree T = ThreadTree();
+    ThreadTree head = nullptr;
+    ThreadTree T = nullptr;
+    // 创建树
+    createTree(&T);
+
+    // 线索化
+    inOrderThreading(&head, T);
+    // 基于线索遍历
+    inOrder(head);
     return 0;
+}
+
+void createTree(ThreadTree *T)
+{
+    ElemType ch = str[inx++];
+    if (ch == '#')
+        *T = nullptr;
+    else
+    {
+        *T = new ThreadNode();
+        (*T)->data = ch;
+        createTree(&(*T)->lchild);
+        if ((*T)->lchild != nullptr)
+            (*T)->ltag = 0;
+        createTree(&(*T)->rchild);
+        if ((*T)->rchild != nullptr)
+            (*T)->rtag = 0;
+    }
+}
+
+void inOrderThreading(ThreadTree *head,ThreadTree T)
+{
+    *head = new ThreadNode();
+    (*head)->ltag = 0;
+    (*head)->rtag = 1;
+    (*head)->rchild = (*head);
+    if (T == nullptr)
+        (*head)->lchild = *head;
+    else
+    {
+        (*head)->lchild = T;
+        pre = (*head);
+        threading(T);
+        pre->rchild = *head;
+        pre->rtag = 1;
+        (*head)->rchild = pre;
+    }
+}
+
+void threading(ThreadTree T)
+{
+    if (T != nullptr)
+    {
+        threading(T->lchild);
+        if (T->lchild == nullptr)
+        {
+            T->ltag = 1;
+            T->lchild = pre;
+        }
+        if (pre->rchild == nullptr)
+        {
+            pre->rtag = 1;
+            pre->rchild = T;
+        }
+        pre = T;
+        threading(T->rchild);
+    }
+}
+
+void inOrder(ThreadTree T)
+{
+    ThreadTree curr;
+    curr = T->lchild;
+    while (curr != T)
+    {
+        while (curr->ltag == 0)
+            curr = curr->lchild;
+        cout << curr->data << " ";
+        while (curr->rtag == 1 && curr->rchild != T)
+        {
+            curr = curr->rchild;
+            cout << curr->data << " ";
+        }
+        curr = curr->rchild;
+    }
+    cout << endl;
 }
 
 //
