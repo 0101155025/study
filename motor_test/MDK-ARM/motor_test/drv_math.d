c90b9abef363275cca63a8c95231914f1d1dfcc9
Fly <13384911233@163.com>
2025-11-13 09:51:55 +0800

    continue studying motor

--- motor_test/MDK-ARM/motor_test/drv_math.d
+++
@@ -1,6 +0,0 @@
-motor_test\drv_math.o: ..\User\drv_math.cpp
-motor_test\drv_math.o: ..\User\drv_math.h
-motor_test\drv_math.o: D:\Keil5\ARM\ARMCC\Bin\..\include\stdint.h
-motor_test\drv_math.o: D:\Keil5\ARM\ARMCC\Bin\..\include\limits.h
-motor_test\drv_math.o: D:\Keil5\ARM\ARMCC\Bin\..\include\math.h
-motor_test\drv_math.o: D:\Keil5\ARM\ARMCC\Bin\..\include\float.h
--- motor_test/MDK-ARM/motor_test/startup_stm32f427xx.d
+++
@@ -1,1 +0,0 @@
-motor_test\startup_stm32f427xx.o: startup_stm32f427xx.s
